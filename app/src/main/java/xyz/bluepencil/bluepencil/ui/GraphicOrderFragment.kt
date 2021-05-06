762b4f643d4cf648da1ab7e5126f2c48e92ee43c
Ayush Dubey <dubeyaayush07@gmail.com>
2021-05-06 19:02:37 +0530

    link

--- app/src/main/java/xyz/bluepencil/bluepencil/ui/GraphicOrderFragment.kt
+++ app/src/main/java/xyz/bluepencil/bluepencil/ui/GraphicOrderFragment.kt
@@ -21,6 +21,7 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import xyz.bluepencil.bluepencil.PaymentActivity
 import java.io.IOException
 
 class GraphicOrderFragment : Fragment() {
@@ -33,9 +34,6 @@ class GraphicOrderFragment : Fragment() {
     companion object {
         const val TAG = "GraphicOrderFragment"
     }
-
-    private val UPI_PAYMENT: Int = 12345
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -63,7 +61,7 @@ class GraphicOrderFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.orderBtn.setOnClickListener {
             if (isOnline()) {
-                payUsingUpi()
+                checkOrderDetails()
             } else {
                 Snackbar.make(binding.root, "Internet Connection Required", Snackbar.LENGTH_LONG)
                     .show()
@@ -72,7 +70,7 @@ class GraphicOrderFragment : Fragment() {
         }
     }
 
-    private fun payUsingUpi() {
+    private fun checkOrderDetails() {
         if (checkOrderCount()) return
         else if (binding.specificationTxt.text.isNullOrBlank()) {
             Snackbar.make(
@@ -92,7 +90,11 @@ class GraphicOrderFragment : Fragment() {
             user!!.freeCount = user!!.freeCount?.minus(1)
             val collection = Firebase.firestore.collection("users")
             collection.document(user!!.id.toString()).update("freeCount", user!!.freeCount)
-                .addOnSuccessListener {  saveOrder(user!!.uid.toString()) }
+                .addOnSuccessListener {
+                    val amount = 0
+                    val order = getOrder()
+                    findNavController().navigate(GraphicOrderFragmentDirections.actionGraphicOrderFragmentToPaymentActivity(order, amount))
+                }
                 .addOnFailureListener {
                     Snackbar.make(
                         binding.root,
@@ -103,21 +105,21 @@ class GraphicOrderFragment : Fragment() {
             return
         }
 
-
         val amount = placard.cost * binding.orderCount.text.toString().toInt()
+        val order = getOrder()
+        findNavController().navigate(GraphicOrderFragmentDirections.actionGraphicOrderFragmentToPaymentActivity(order, amount))
+    }
 
-        val uri = Uri.parse("upi://pay").buildUpon()
-            .appendQueryParameter("pa", "9131455619@okbizaxis")
-            .appendQueryParameter("pn", placard.userName)
-            .appendQueryParameter("tn", "Order Payment")
-            .appendQueryParameter("am", "$amount")
-            .appendQueryParameter("cu", "INR")
-            .build()
-        val upiPayIntent = Intent(Intent.ACTION_VIEW)
-        upiPayIntent.data = uri
-        val chooser = Intent.createChooser(upiPayIntent, "Pay with")
-        startActivityForResult(chooser, UPI_PAYMENT)
-
+    private fun getOrder(): Order {
+        return Order(
+            userId = user!!.uid.toString(),
+            editorId = placard.userId,
+            count = binding.orderCount.text.toString().toInt(),
+            remark = binding.specificationTxt.text.toString(),
+            link = binding.linkTxt.text.toString(),
+            type = placard.type,
+            editorName = placard.userName
+        )
     }
 
     private fun checkOrderCount(): Boolean {
@@ -139,103 +141,6 @@ class GraphicOrderFragment : Fragment() {
         }
         return false
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == UPI_PAYMENT) {
-                if (data != null) {
-                    val trxt = data.getStringExtra("response")
-                    Log.e("UPI", "onActivityResult: $trxt")
-                    val dataList: ArrayList<String?> = ArrayList()
-                    dataList.add(trxt)
-                    upiPaymentDataOperation(dataList)
-                } else {
-                    Log.e("UPI", "onActivityResult: " + "Return data is null")
-                    val dataList: ArrayList<String?> = ArrayList()
-                    dataList.add("nothing")
-                    upiPaymentDataOperation(dataList)
-                }
-            }
-        } else {
-            Snackbar.make(binding.root, "Operation Failed", Snackbar.LENGTH_LONG)
-                .show()
-        }
-    }
-
-    private fun upiPaymentDataOperation(data: ArrayList<String?>) {
-        if (true) {
-            var str = data[0]
-            Log.e("UPIPAY", "upiPaymentDataOperation: $str")
-            var paymentCancel = ""
-            if (str == null) str = "discard"
-            var status = ""
-            var approvalRefNo = ""
-            val response = str.split("&").toTypedArray()
-            for (i in response.indices) {
-                val equalStr = response[i].split("=").toTypedArray()
-                if (equalStr.size >= 2) {
-                    if (equalStr[0].toLowerCase() == "Status".toLowerCase()) {
-                        status = equalStr[1].toLowerCase()
-                    } else if (equalStr[0].toLowerCase() == "ApprovalRefNo".toLowerCase() || equalStr[0].toLowerCase() == "txnRef".toLowerCase()) {
-                        approvalRefNo = equalStr[1]
-                    }
-                } else {
-                    paymentCancel = "Payment cancelled by user."
-                }
-            }
-            if (status == "success") {
-                //Code to handle successful transaction here.
-                saveOrder(user!!.uid.toString())
-                Log.e("UPI", "payment successfull: $approvalRefNo")
-            } else if ("Payment cancelled by user." == paymentCancel) {
-                Snackbar.make(binding.root, "Payment cancelled by user.", Snackbar.LENGTH_SHORT)
-                    .show()
-                Log.e("UPI", "Cancelled by user: $approvalRefNo")
-            } else {
-                Snackbar.make(
-                    binding.root,
-                    "Transaction failed. Please try again",
-                    Snackbar.LENGTH_SHORT
-                ).show()
-                Log.e("UPI", "failed payment: $approvalRefNo")
-            }
-        } else {
-            Log.e("UPI", "Internet issue: ")
-            Snackbar.make(
-                binding.root,
-                "Internet connection is not available. Please check and try again",
-                Snackbar.LENGTH_SHORT
-            ).show()
-        }
-    }
-
-    private fun saveOrder(userId: String) {
-        val collection = Firebase.firestore.collection("orders")
-
-        val order = Order(
-            userId = userId,
-            editorId = placard.userId,
-            count = binding.orderCount.text.toString().toInt(),
-            remark = binding.specificationTxt.text.toString(),
-            link = binding.linkTxt.text.toString(),
-            type = placard.type,
-            editorName = placard.userName
-        )
-        val task = collection.add(order)
-
-        task.addOnSuccessListener {
-            Snackbar.make(binding.root, "Order submitted successfully", Snackbar.LENGTH_LONG)
-                .show()
-            findNavController().navigate(GraphicOrderFragmentDirections.actionGraphicOrderFragmentToInboxFragment())
-        }
-
-        task.addOnFailureListener {
-            Snackbar.make(binding.root, "Failed to take your order please contact support", Snackbar.LENGTH_LONG)
-                .show()
-        }
-    }
-
 
     private fun isOnline(): Boolean {
         val runtime = Runtime.getRuntime()
